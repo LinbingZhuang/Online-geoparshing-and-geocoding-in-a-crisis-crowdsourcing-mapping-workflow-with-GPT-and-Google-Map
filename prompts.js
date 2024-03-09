1ad529c09e9e64536c98de401a68d0a4dbeac9b6
Linbing1065 <126106057+Linbing1065@users.noreply.github.com>
2024-03-09 17:15:47 +0100

    Create prompts.js

---
+++ prompts.js
@@ -0,0 +1,8 @@
+export const prompts = {
+    locationExtraction: [
+        {
+            role: "system",
+            content: "Understand the most specific location from the user's inputs, which are tweets about Hurricane Harvey. Most of the locations should be within the Houston area in Texas, USA."
+        }
+    ]
+};
